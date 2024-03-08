9afb9241b70af75aed1e3209c22cfb3e26c17b0d
xc112lg <xc112lg@users.noreply.github.com>
2024-03-08 07:09:55 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -8,6 +8,8 @@ crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
+              #Weekly Update
+#https://github.com/xc112lg/crdroid10.x/releases
 mkdir -p cc;rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh all build delzip
 "
 . a.sh
