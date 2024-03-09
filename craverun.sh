81119710d0898770496886cacedd83f95f275b08
xc112lg <xc112lg@users.noreply.github.com>
2024-03-09 19:32:30 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,9 +4,11 @@ git clone https://$GH_TOKEN@github.com/xc112lg/crdroid10.x.git
 cp -f crdroid10.x/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "         
-#testbuild weekly ota update
-#https://github.com/xc112lg/crdroid10.x/releases
-mkdir -p cc;rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh all build delzip
+crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+              #Thanks to UV n Team
+        #no dual account just to build faster
+              #respect the rule
+
+mkdir -p cc;rm -rf scripts prebuilts/clang/host/linux-x86&& git clone https://github.com/xc112lg/scripts.git -b evot&&repo init --depth 1 -u https://github.com/Evolution-X/manifest -b udc&&  chmod u+x scripts/sync.sh && . scripts/sync.sh
 "
 . a.sh
