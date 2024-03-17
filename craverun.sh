b7cac77718fc42a19d56909323aa0f5a1423e89b
xc112lg <xc112lg@users.noreply.github.com>
2024-03-17 04:03:54 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,11 +4,11 @@ git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
 cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
 
-rm -rf scripts;repo init --depth 1 -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs;git clone https://github.com/xc112lg/scripts.git -b aosp;chmod u+x scripts/sync.sh;bash scripts/sync.sh h872 build delzip aosp aosp m bacon 32
+mkdir -p cc;rm -rf scripts;repo init --depth 1 -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs;git clone https://github.com/xc112lg/scripts.git -b aosp;chmod u+x scripts/sync.sh;bash scripts/sync.sh h872 build delzip aosp aosp m bacon 32
 "
-. craverun.sh
+. a.sh
