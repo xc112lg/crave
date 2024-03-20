26eb3e110466ee06717847358b7f775f4cd4a45d
xc112lg <xc112lg@users.noreply.github.com>
2024-03-20 21:56:32 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-rm -rf crdroid10.x
-git clone https://$GH_TOKEN@github.com/xc112lg/crdroid10.x.git
-cp -f crdroid10.x/a.sh a.sh
+rm -rf Evolution-X
+git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
+cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave ssh -- touch test.txt
-crave ssh -- /opt/crave/resync.sh 
+crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+              #Thanks to UV n Team
+        #no dual account just to build faster
+              #respect the rule
+
+rm -rf scripts;repo init --depth 1 -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs;git clone https://github.com/xc112lg/scripts.git -b aosp;chmod u+x scripts/sync.sh;bash scripts/sync.sh all build delzip aosp aosp m bacon 32
+"
+. a.sh
