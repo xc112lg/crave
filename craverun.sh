dd0aa7f1bb9e2b5029229bc4d373f5cb6b4767f6
xc112lg <xc112lg@users.noreply.github.com>
2024-03-20 07:40:03 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,7 +4,7 @@ git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
 cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
