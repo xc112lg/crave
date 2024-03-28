5aae69776ea2174f831661fc3396a0dbd7b53c31
xc112lg <xc112lg@users.noreply.github.com>
2024-03-28 20:45:28 +0000

    Update run.sh with commands

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 export GH_TOKEN=$(cat gh_token.txt)
 gh auth login --with-token $GH_TOKEN
 gh auth status
-mkdir -p DerpFest13
+mkdir -p LineageOS20
 bp=$(cat bp.txt)
 cp -f crave/run1.sh run1.sh
 chmod +x run1.sh
-cp -f crave/craverun.sh DerpFest13
+cp -f crave/craverun.sh LineageOS20
 tmux set-environment bp "$bp"
 tmux kill-session -t $bp
 tmux new-session -d -s $bp
