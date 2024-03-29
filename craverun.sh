8963798b586259197951033d1ba29b48c0f3143c
xc112lg <xc112lg@users.noreply.github.com>
2024-03-29 00:22:32 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-rm -rf crdroid10.x
-git clone https://$GH_TOKEN@github.com/xc112lg/crdroid10.x.git
-cp -f crdroid10.x/a.sh a.sh
+rm -rf Evolution-X
+git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
+cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "         
-#testbuild weekly ota update
-#https://github.com/xc112lg/crdroid10.x/releases
-mkdir -p cc;rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh all build delzip
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
+              #Thanks to UV n Team
+        #no dual account just to build faster
+              #respect the rule
+
+rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b aosp;repo init -u https://github.com/Evolution-X/manifest -b uqpr2;chmod u+x scripts/sync.sh ;bash scripts/sync.sh h872 build delzip evolution evolution m evolution 32
 "
 . a.sh
