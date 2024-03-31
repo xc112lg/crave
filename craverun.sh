a9c1fd0c9a9464fffd296d0244e55e7cc4609b06
xc112lg <xc112lg@users.noreply.github.com>
2024-03-31 23:15:53 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,5 +4,9 @@ git clone https://$GH_TOKEN@github.com/xc112lg/crdroid10.x.git
 cp -f crdroid10.x/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave ssh -- touch test.txt
-crave ssh -- /opt/crave/resync.sh 
+crave run --no-patch  -- "         
+#testbuild weekly ota update
+#https://github.com/xc112lg/crdroid10.x/releases
+mkdir -p cc;rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh h872 build delzip lineage -ap1a lineage m bacon 64
+"
+. a.sh
