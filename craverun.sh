fe02a9686bc78ea97ac79e5d8df5808e66881430
xc112lg <xc112lg@users.noreply.github.com>
2024-04-04 10:08:43 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-rm -rf crdroid10.x
-git clone https://$GH_TOKEN@github.com/xc112lg/crdroid10.x.git
-cp -f crdroid10.x/a.sh a.sh
+rm -rf Evolution-X
+git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
+cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave ssh -- touch test.txt
-crave ssh -- /opt/crave/resync.sh 
+crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+              #Thanks to UV n Team
+        #no dual account just to build faster
+              #respect the rule
+
+mkdir -p cc;rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh h872 build delzip lineage -ap1a lineage m bacon 128
+"
+. a.sh
