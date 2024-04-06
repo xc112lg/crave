a0fb2ffba71a91a84fabea314cfa7b7b3541d243
xc112lg <xc112lg@users.noreply.github.com>
2024-04-06 23:08:12 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-rm -rf Evolution-X
-git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
-cp -f Evolution-X/a.sh a.sh
+rm -rf crdroid10.x
+git clone https://$GH_TOKEN@github.com/xc112lg/crdroid10.x.git
+cp -f crdroid10.x/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
-              #Thanks to UV n Team
-        #no dual account just to build faster
-              #respect the rule
-
-mkdir -p cc;rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b mi;chmod u+x scripts/sync.sh;bash scripts/sync.sh
-"
-. a.sh
+crave ssh -- touch test.txt
+crave ssh -- /opt/crave/resync.sh 
