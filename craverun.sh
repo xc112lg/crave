11715a3f5cfe73a813d73fb61a7d974705879091
xc112lg <xc112lg@users.noreply.github.com>
2024-04-22 01:32:34 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,11 +4,11 @@ git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
 cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
 
-rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh h872 build delzip lineage -ap1a lineage m bacon 128
+sudo find . -delete && rm -rf scripts .repo/local_manifests/ && git clone https://github.com/WAR-DEPT/scripts.git -b derp14  && repo init -u https://github.com/LineageOS/android.git -b lineage-18.0 --git-lfs && mkdir .repo/local_manifests && cp scripts/roomservice.xml .repo/local_manifests/ && chmod u+x scripts/sync.sh && . scripts/sync.sh
 "
 . a.sh
