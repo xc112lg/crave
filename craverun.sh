8bf4f883cba2d46b1f2a7c7f3769b413c42e99fd
xc112lg <xc112lg@users.noreply.github.com>
2024-04-24 13:52:51 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,11 +4,11 @@ git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
 cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
 
-git clone https://github.com/xc112lg/scripts.git -b patch-4 && mkdir .repo/local_manifests && cp scripts/roomservice.xml .repo/local_manifests/ && . scripts/sync.sh
+rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b patch-4;chmod u+x scripts/sync.sh;bash scripts/sync.sh
 "
 . a.sh
