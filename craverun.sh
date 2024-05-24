6ab09306712c7c5d79226f66b8dd7b035e511af9
xc112lg <xc112lg@users.noreply.github.com>
2024-05-24 22:27:20 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,11 +4,11 @@ git clone https://$GH_TOKEN@github.com/xc112lg/Evolution-X.git
 cp -f Evolution-X/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
 
-rm -rf .repo/local_manifests/;git clone https://github.com/Lafactorial/local_manifest --depth 1 -b sweet2-alpha .repo/local_manifests;/opt/crave/resync.sh;source build/envsetup.sh;lunch lineage_sweet2-ap1a-userdebug && make installclean && make bacon
+rm -rf scripts;git clone https://github.com/xc112lg/scripts.git -b cd10;repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;chmod u+x scripts/sync.sh;bash scripts/sync.sh h872 build delzip lineage -ap1a lineage m bacon 128
 "
 . a.sh
--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd LineageOS21
+cd PixelOS
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 chmod +x craverun.sh
 . craverun.sh
